88aa8ea43bec932d8ded1a068d39716db9b2b8cf
llp0702 <massyldjemai@gmail.com>
2021-02-12 18:02:05 +0100

    encore un test

--- buildsite/src/test/java/ParserForSiteTest.java
+++ buildsite/src/test/java/ParserForSiteTest.java
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import nu.validator.checker.Checker;
+
+import java.util.zip.CheckedInputStream;
 
 public class ParserForSiteTest {
     @Test
     public void testTest(){
         Assertions.assertEquals(1,1);
+
     }
 
 }
--- md2html/src/main/java/SimpleParser.java
+++ md2html/src/main/java/SimpleParser.java
@@ -2,8 +2,12 @@ import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
+import java.io.FileNotFoundException;
+
 public class SimpleParser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        HTML5Validator v = new HTML5Validator();
+        v.validate();
         Parser parser = Parser.builder().build();
         Node document = parser.parse("This is *Sparta*");
         HtmlRenderer renderer = HtmlRenderer.builder().build();
