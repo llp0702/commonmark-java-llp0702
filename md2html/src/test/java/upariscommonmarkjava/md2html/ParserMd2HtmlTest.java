17c83c0f5754c4cece3aeede0538f56b762078da
llp0702 <massyldjemai@gmail.com>
2021-02-26 15:30:16 +0100

    Modification gitignore

--- md2html/src/test/java/upariscommonmarkjava/md2html/ParserMd2HtmlTest.java
+++ md2html/src/test/java/upariscommonmarkjava/md2html/ParserMd2HtmlTest.java
@@ -66,49 +66,5 @@ class ParserMd2HtmlTest {
         return CMFile.fromString(new String(inputStreamInput.readAllBytes()));
     }
 
-    @AllArgsConstructor
-    public static class HTML5Validator {
-        private final InputStream document;
-
-        private final MyHtml5ValidatorErrorHandler errorHandler = new MyHtml5ValidatorErrorHandler();
-
-        public boolean validate() {
-
-            SimpleDocumentValidator validator = new SimpleDocumentValidator();
-            String schemaUrl = "http://s.validator.nu/html5-all.rnc";
-
-            InputSource source = new InputSource(document);
-            try {
-                validator.setUpMainSchema(schemaUrl, errorHandler);
-                validator.setUpValidatorAndParsers(errorHandler, false, false);
-                validator.checkHtmlInputSource(source);
-            } catch (Exception e) {
-                return false;
-            }
-            return !errorHandler.hasErrors();
-        }
-
-        private static class MyHtml5ValidatorErrorHandler implements ErrorHandler {
-            int err = 0;
-
-            @Override
-            public void warning(SAXParseException exception) throws SAXException {
-            }
-
-            @Override
-            public void error(SAXParseException exception) throws SAXException {
-                err++;
-            }
-
-            @Override
-            public void fatalError(SAXParseException exception) throws SAXException {
-                err++;
-            }
-
-            public boolean hasErrors() {
-                return err > 0;
-            }
-        }
-    }
 
 }
