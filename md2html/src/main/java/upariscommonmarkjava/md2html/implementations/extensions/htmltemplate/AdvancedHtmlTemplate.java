f79a729873c39e03d4298022d7c557883471a4a7
penjenaire56 <nathan.caracciolo@free.fr>
2021-03-18 11:28:43 +0100

    Ajout du IF dans template++

--- md2html/src/main/java/upariscommonmarkjava/md2html/implementations/extensions/htmltemplate/AdvancedHtmlTemplate.java
+++ md2html/src/main/java/upariscommonmarkjava/md2html/implementations/extensions/htmltemplate/AdvancedHtmlTemplate.java
@@ -9,11 +9,13 @@ import java.util.regex.Matcher;
 
 @SuperBuilder
 public class AdvancedHtmlTemplate extends HtmlTemplate {
-    protected static final String PATTERN_FOR = "\\{%[ ]*for[ ]+(.*?)[ ]+in[ ]+(.*?)[ ]*%\\}((\\r|\\n|.)*?)\\{% endfor %\\}";
+    protected static final String PATTERN_FOR = "\\{%[ ]*for[ ]+(.*?)[ ]+in[ ]+(.*?)[ ]*%\\}((\\r|\\n|.)*?)\\{%[ ]*endfor[ ]*%\\}";
+    protected static final String PATTERN_IF = "\\{%[ ]*if[ ]+(.*?)[ ]*%\\}((\\r|\\n|.)*?)\\{%[ ]*endif[ ]*%\\}";
 
     @Override
     public String apply() {
         this.templateContent = matchAndReplace(PATTERN_FOR, templateContent, this::replaceFor);
+        this.templateContent = matchAndReplace(PATTERN_IF, templateContent, this::replaceIf);
         return super.apply();
     }
 
@@ -38,7 +40,7 @@ public class AdvancedHtmlTemplate extends HtmlTemplate {
 
         final StringBuilder sb = new StringBuilder(innerContent.length());
 
-        for (final Object tomlObject : getIterableTomlList(super.getMetadata(array))) {
+        for (final Object tomlObject : getIterableTomlList(getMetadata(array))) {
             sb.append(tomlObject instanceof TomlTable ?
                     matchForTomlTable(element, innerContent, (TomlTable) tomlObject) :
                     matchAndReplace("\\{\\{[ ]*" + element + "[ ]*\\}\\}", innerContent ,
@@ -46,6 +48,20 @@ public class AdvancedHtmlTemplate extends HtmlTemplate {
         }
 
         return sb.toString();
+    }
+
+    private String replaceIf(Matcher matcher)
+    {
+        final String cond = matcher.group(1).trim();
+
+        if(getMetadata(cond).toString().equals("true")) {
+            return matcher.group(2).trim();
+        }
+        else if(!getMetadata(cond).toString().equals("false")) {
+            logger.warning("The value is not a boolean");
+        }
+
+        return "";
     }
 
     private String matchForTomlTable(String element, String innerContent, TomlTable table) {
