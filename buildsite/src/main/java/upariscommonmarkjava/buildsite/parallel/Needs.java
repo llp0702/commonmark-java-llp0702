a9a8cf26b030918da9baa5a89fbc43e320b201e7
Nathan_Caracciolo <nathan.caracciolo@free.fr>
2021-04-02 13:59:06 +0000

    Parallel

---
+++ buildsite/src/main/java/upariscommonmarkjava/buildsite/parallel/Needs.java
@@ -0,0 +1,42 @@
+package upariscommonmarkjava.buildsite.parallel;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public final class Needs<Type> {
+    private final Type value;
+    private final List<Type> valusNeeded;
+
+    public static <Type> List<Type> toArrayList(Type[] array){
+        final ArrayList<Type> elements = new ArrayList<>();
+        Collections.addAll(elements, array);
+        return elements;
+    }
+
+    public Needs(final Type value, final List<Type> valusNeeded){
+        this.value = value;
+        this.valusNeeded = valusNeeded;
+    }
+
+    public Needs(final Type value, final Type... constraints){
+        this(value,toArrayList(constraints));
+    }
+
+    public final Type getValue(){
+        return this.value;
+    }
+
+    public final boolean noConstraint(){
+        return this.valusNeeded.isEmpty();
+    }
+
+    public final Iterator<Type> constraintIterator(){
+        return this.valusNeeded.iterator();
+    }
+
+    public final void removeConstraint(final Type c){
+        this.valusNeeded.remove(c);
+    }
+}
