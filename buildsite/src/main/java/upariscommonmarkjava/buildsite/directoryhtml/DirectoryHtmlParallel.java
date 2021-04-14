1f6a546875727559f3f34c0893a6fc8bf2a3313d
penjenaire56 <nathan.caracciolo@free.fr>
2021-04-14 19:48:57 +0200

    Developpement de la fonctionnalite parallel appliqué à DirectoryHtml

--- buildsite/src/main/java/upariscommonmarkjava/buildsite/directoryhtml/DirectoryHtmlParallel.java
+++ buildsite/src/main/java/upariscommonmarkjava/buildsite/directoryhtml/DirectoryHtmlParallel.java
@@ -1,2 +1,83 @@
-package upariscommonmarkjava.buildsite.directoryhtml;public class DirectoryHtmlParallel {
+package upariscommonmarkjava.buildsite.directoryhtml;
+
+import lombok.NonNull;
+import upariscommonmarkjava.buildsite.parallel.IndependantQueue;
+import upariscommonmarkjava.buildsite.parallel.Needs;
+import upariscommonmarkjava.buildsite.parallel.NeedsException;
+import upariscommonmarkjava.buildsite.theme.ITheme;
+import upariscommonmarkjava.md2html.implementations.incremental.Hierarchie;
+import upariscommonmarkjava.md2html.interfaces.ITOMLFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+
+public class DirectoryHtmlParallel extends DirectoryHtml{
+
+    private int nb_thread;
+
+    public DirectoryHtmlParallel(@NonNull final Path inputContentBasePath, @NonNull final ITOMLFile tomlOptions,
+                                 @NonNull final List<Path> mdFilesPaths, @NonNull final List<Path> staticFilesPaths,
+                                 @NonNull final List<Path> templatesPaths, @NonNull final Optional<ITheme> theme){
+        super(inputContentBasePath,tomlOptions,mdFilesPaths,staticFilesPaths,templatesPaths,theme);
+    }
+
+    private List<List<Path>> buildIndependantQueue() throws NeedsException {
+        final HashMap<String, Needs<Path>> tableConstraints = new HashMap<>();
+
+        for(final Path staticFile : this.staticFilesPaths){
+            refreshHash(staticFile);
+            tableConstraints.put(staticFile.toString(), new Needs<>(staticFile));
+        }
+
+        for(final Path inputMdFile: inputFilesMdPaths) {
+            refreshHash(inputMdFile);
+            tableConstraints.put(inputMdFile.toString(), new Needs<>(inputMdFile));
+        }
+
+        for(final Path inputMdFile: inputFilesMdPaths) {
+            final Optional<Hierarchie> hier = convertMd2HtmlGethierarchy(inputMdFile);
+            if(hier.isPresent())
+            {
+                for(final String dep : hier.get().getDepCourant(inputMdFile.toString())) {
+                    tableConstraints.get(dep).addNeededValue(inputMdFile);
+                }
+            }
+        }
+        /*
+        TODO
+            applyToValid(optTheme, theme -> {
+            try {
+                for(final Path staticFile: theme.getStaticPaths()){
+                    copyStaticFiles(targetBasePath ,theme.getBasePath().resolve("static"), false, staticFile);
+                }
+            } catch (IOException e) {
+                logger.warning("Exception when trying to copy static files");
+                e.printStackTrace();
+            }});
+        */
+        final List<Needs<Path>> constraints = new ArrayList<>();
+        for(final String key : tableConstraints.keySet()) {
+            constraints.add(tableConstraints.get(key));
+        }
+        return IndependantQueue.generate(constraints);
+    }
+    @Override
+    protected void save(@NonNull final Path targetBasePath) throws IOException {
+        createFolder(targetBasePath);
+        try {
+            final List<List<Path>> indQueue = buildIndependantQueue();
+            for(final List<Path> list : indQueue){
+                System.out.println(list);
+            }
+        }catch(NeedsException needsException){
+
+        }
+
+        saveGlobalHierarchie(targetBasePath);
+    }
+
+    public void setNbThread(final int nb_thread){
+        this.nb_thread = nb_thread;
+    }
 }
--- md2html/src/main/java/upariscommonmarkjava/md2html/implementations/ConverterMd2Html.java
+++ md2html/src/main/java/upariscommonmarkjava/md2html/implementations/ConverterMd2Html.java
@@ -75,9 +75,7 @@ public class ConverterMd2Html implements IConverterMd2Html {
             return "";
 
         final String htmlContent = htmlRenderer.render(resNode);
-        if(globalMetadata.isPresent()){
-            
-        }
+
         if (templateFiles.isEmpty())
             return wrapHtmlBody(htmlContent);
 
@@ -100,33 +98,56 @@ public class ConverterMd2Html implements IConverterMd2Html {
         return "<!DOCTYPE HTML><html lang=\"en\"><head><title>title</title></head><body>" + body + "</body></html>";
     }
 
-    private String applyTemplateIfPresent(@NonNull final ICMFile cmFile, final String htmlContent){
+    public Optional<Hierarchie> getActualHierarchie(@NonNull final ICMFile cmFile){
         final List<TomlTable> metaDataLocal = cmFile.getTomlMetadataLocal();
-        Optional<Path> template = searchPathEqual(templateFiles,"default.html");
+        final Optional<Path> template = searchTemplate(metaDataLocal);
+        refreshHierarchie(cmFile,template);
+        return this.actualHierarchie;
+    }
 
-        for (TomlTable metaData : metaDataLocal) {
-            if (metaData != null) {
-                String curRes = metaData.getString("template");
-                if (curRes != null) {
-                    template = searchPathEndsWith(templateFiles,curRes);
-                    if (template.isEmpty())
-                        break;
+    private void refreshHierarchie(@NonNull final ICMFile cmFile, final Optional<Path> template){
+        if(template.isEmpty())
+            return;
+
+        try {
+            if (actualHierarchie.isPresent()) {
+                //retire de toute la liste des templates le cmFile afin de mettre le potentiel nouveau
+                actualHierarchie.get().supprInstanceOfCourant(cmFile.getStringPath(), templateFiles);
+                actualHierarchie.get().addDep(template.get().toString(), cmFile.getStringPath());
+            }
+        } catch (IOException ignored) {}
+    }
+
+    private Optional<Path> searchTemplate(final List<TomlTable> metaDataLocal){
+        Optional<Path> template = searchPathEqual(templateFiles,"default.html");
+        if(metaDataLocal != null){
+            for (TomlTable metaData : metaDataLocal) {
+                if (metaData != null) {
+                    String curRes = metaData.getString("template");
+                    if (curRes != null) {
+                        template = searchPathEndsWith(templateFiles,curRes);
+                        if (template.isEmpty())
+                            break;
+                    }
                 }
             }
         }
+        return template;
+    }
+
+    private String applyTemplateIfPresent(@NonNull final ICMFile cmFile, final String htmlContent){
+        final List<TomlTable> metaDataLocal = cmFile.getTomlMetadataLocal();
+        final Optional<Path> template = searchTemplate(metaDataLocal);
 
         if (template.isEmpty()) {
             return wrapHtmlBody(htmlContent);
         }
 
+        refreshHierarchie(cmFile,template);
+
         String file = "";
         try{
             file = Files.readString(template.get());
-            if(actualHierarchie.isPresent()){
-                //retire de toute la liste des templates le cmFile afin de mettre le potentiel nouveau
-                actualHierarchie.get().supprInstanceOfCourant(cmFile.getStringPath(), templateFiles);
-                actualHierarchie.get().addDep(template.get().toString(), cmFile.getStringPath());
-            }
         }
         catch(IOException ioe){
             logger.warning(ioe.getMessage());
