013a791eafa25f050689c915b6feb4bb33ceccd4
penjenaire56 <nathan.caracciolo@free.fr>
2021-04-18 19:35:57 +0200

    correction lien fichier

--- md2html/src/test/java/upariscommonmarkjava/md2html/MysteryTemplateTest.java
+++ md2html/src/test/java/upariscommonmarkjava/md2html/MysteryTemplateTest.java
@@ -9,6 +9,6 @@ public class MysteryTemplateTest extends HtmlTemplateTest{
     @Test
     void mysteryTemplateTest(){
         assertEquals("_list_files_1",getResult("testListFiles"));
-        assertEquals("fil1::file3::test2:://test/fil1::test2/test2::test3/test4/file3::",getResult("testListFilesFor"));
+        assertEquals("test2::fil1::file3:://test/fil1::test2/test2::test3/test4/file3::",getResult("testListFilesFor"));
     }
 }
