2288eb487552322b08aebb2cc05a8d3f027bf203
Nathan_Caracciolo <nathan.caracciolo@free.fr>
2021-04-21 19:30:10 +0000

    Refacto

--- md2html/src/test/java/upariscommonmarkjava/md2html/MysteryTemplateTest.java
+++ md2html/src/test/java/upariscommonmarkjava/md2html/MysteryTemplateTest.java
@@ -3,12 +3,25 @@ package upariscommonmarkjava.md2html;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MysteryTemplateTest extends HtmlTemplateTest{
 
     @Test
     void mysteryTemplateTest(){
         assertEquals("_list_files_1",getResult("testListFiles"));
-        //assertEquals("test2::fil1::file3:://test/fil1::test2/test2::test3/test4/file3::",getResult("testListFilesFor"));
+        final String[] result = getResult("testListFilesFor").split("//");
+
+        System.out.println(result);
+        assertTrue(result[0].contains("test2::")
+                && result[0].contains("fil1::")
+                && result[0].contains("file3::")
+                && result[1].contains("test")
+                && result[1].contains("fil1")
+                && result[1].contains("test2")
+                && result[1].contains("test3")
+                && result[1].contains("test4")
+                && result[1].contains("file3"));
+
     }
 }
